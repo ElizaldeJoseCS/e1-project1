469d84ec083472d05458b9c4636b34b953f910e8
Jose Elizalde <125120357+ElizaldeJoseCS@users.noreply.github.com>
2026-01-15 17:24:37 -0800

    Rigidbody stuff

---
+++ E1Uni2D/Assets/JumpScript.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class JumpScript : MonoBehaviour
+{
+
+    bool isGrounded;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+}
--- E1Uni2D/Assets/PlayerController.cs
+++ E1Uni2D/Assets/PlayerController.cs
@@ -1,12 +1,15 @@
-
 using UnityEngine;
 using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     float movementX;
     float movementY;
+    Rigidbody2D rb;
+    bool isGrounded = false;
 
-    [SerializeField] float speed = 5f;
+    [SerializeField] float speed = 10f;
+    [SerializeField] float jumpPower = 100f;
+
 
     void OnMove(InputValue value)
     {
@@ -19,16 +22,38 @@ public class PlayerController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
-
 
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        float movementDistanceX = movementX * speed * Time.deltaTime;
-        float movementDistanceY = movementY * speed * Time.deltaTime;
-        transform.position = new Vector2(transform.position.x + movementDistanceX, transform.position.y + movementDistanceY);
+        // float movementDistanceX = movementX * speed * Time.deltaTime;
+        // float movementDistanceY = movementY * speed * Time.deltaTime;
+        // transform.position = new Vector2(transform.position.x + movementDistanceX, transform.position.y + movementDistanceY);
+        rb.linearVelocity = new Vector2(movementX * speed, rb.linearVelocity.y);
+
+
+        if (movementY > 0 && isGrounded)
+        {
+            rb.AddForce(new Vector2(0, jumpPower));
+        }
+
     }
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+    void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+
 }
